3a9bcc222ccc181767cf74fdb17a33e7e3ed7156
Konrad Dysput <konrad.dysput@gmail.com>
2018-12-08 16:12:40 +0100

    Initial commit

---
+++ ServiceBusSample.Consumer/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServiceBusSample.Consumer
+{
+    internal class Program
+    {
+        private const string CONNECTION = "{connectionString}";
+
+        private const string QUEUE_NAME = "{queueName}";
+
+        private readonly int iterator = 0;
+        private readonly QueueClient _queueClient;
+
+        public Program()
+        {
+            _queueClient = new QueueClient(CONNECTION, QUEUE_NAME);
+        }
+
+        public void Listen()
+        {
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+
+            _queueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+
+        }
+
+        private async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
+
+            await _queueClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        private Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+            var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {context.Endpoint}");
+            Console.WriteLine($"- Entity Path: {context.EntityPath}");
+            Console.WriteLine($"- Executing Action: {context.Action}");
+            return Task.CompletedTask;
+        }
+
+        public static void Main(string[] args)
+        {
+            var program = new Program();
+            program.Listen();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ServiceBusSample/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceBusSample
+{
+    internal class Program
+    {
+        private readonly QueueClient _queueClient;
+        private int iterator = 0;
+
+        private const string CONNECTION = "{ConnectionString}";
+        private const string QUEUE_NAME = "{QueueName}";
+
+        public Program()
+        {
+            _queueClient = new QueueClient(CONNECTION, QUEUE_NAME);
+        }
+
+        public async Task SendMessage()
+        {
+            string body = $"Message body {iterator++}";
+            var message = new Message(Encoding.UTF8.GetBytes(body));
+
+            await _queueClient.SendAsync(message);
+        }
+
+        public static async Task Main(string[] args)
+        {
+            var program = new Program();
+            Console.WriteLine("Producent program");
+            Console.WriteLine("Sending messages to consumer");
+            while (true)
+            {
+                await program.SendMessage();
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
